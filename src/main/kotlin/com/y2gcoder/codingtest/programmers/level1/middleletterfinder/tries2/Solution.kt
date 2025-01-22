3d2fe431cef48f16605e3c1590a4eb8ffda9eadc
y2gcoder <y2gcoder@gmail.com>
2025-01-22 23:14:34 +0900

    feature(가운데 글자 가져오기): 제출

---
+++ src/main/kotlin/com/y2gcoder/codingtest/programmers/level1/middleletterfinder/tries2/Solution.kt
@@ -0,0 +1,12 @@
+package com.y2gcoder.codingtest.programmers.level1.middleletterfinder.tries2
+
+/**
+ * 가운데 글자 가져오기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/12903?language=kotlin
+ * 다른 아이디어: 인덱스가 아니라 길이 기준으로 하면 ((길이 - 1) / 2)..(길이 / 2) 범위로도 되나?
+ */
+class Solution {
+    fun solution(s: String): String {
+        return s.slice(((s.length - 1) / 2)..(s.length / 2))
+    }
+}
---
+++ src/test/kotlin/com/y2gcoder/codingtest/programmers/level1/middleletterfinder/tries/SolutionTest.kt
@@ -0,0 +1,19 @@
+package com.y2gcoder.codingtest.programmers.level1.middleletterfinder.tries
+
+import io.kotest.core.spec.style.FunSpec
+import io.kotest.data.forAll
+import io.kotest.data.row
+import io.kotest.matchers.shouldBe
+
+class SolutionTest : FunSpec({
+    context("단어의 가운데 글자를 구할 수 있다") {
+        forAll(
+            row("abcde", "c"),
+            row("qwer", "we"),
+        ) { s: String, expected: String ->
+            val sut = Solution()
+
+            sut.solution(s) shouldBe expected
+        }
+    }
+})
